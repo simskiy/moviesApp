f04da2a90ce5b8c1c1719e1d0b3447a2cfe50050
simskiy <simskiy@gmail.com>
2022-09-25 14:51:04 +0300

    Инициализация проекта

---
+++ README.md
@@ -0,0 +1,1 @@
+# Для просмотра приложения пройдите по ссылке https://todo-app-henna-sigma.vercel.app/
---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input } from 'antd'
+
+// const API_KEY = '26a5985902ec1ca50921e555b3baaab3'
+
+export default class App extends Component {
+  render() {
+    return <Input placeholder="Type to search..." />
+  }
+}
